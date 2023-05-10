c2a2286b3338e09d4c5c282153d056458799b88f
Pokrywkaa <damian.pokrywka3@gmail.com>
2023-05-10 19:05:18 +0200

    add text and image recognition

--- gallery_app/components/Home.js
+++ gallery_app/components/Home.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import TextRecognition from 'react-native-text-recognition';
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 const Home = (props) => {
     const [type, setType] = useState(Camera.Constants.Type.back);
@@ -9,11 +12,38 @@ const Home = (props) => {
     const [photos, setPhotos] = useState([]);
     const cameraRef = useRef(null);
 
+    const [predictions, setPredictions] = useState([]);
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+      const loadModel = async () => {
+        await tf.ready();
+        const model = await mobilenet.load();
+        const imageTensor = tf.zeros([1, 224, 224, 3]);
+        const prediction = await model.predict(imageTensor);
+        console.log(prediction);
+      };
+      loadModel();
+    }, []);
+
+
     const handleTakePhoto = async () => {
         if (cameraRef.current) {
           const options = { quality: 0.5, base64: true };
           const data = await cameraRef.current.takePictureAsync(options);
+          // Detect image
+          const imageTensor = tf.image.decodeJpeg(tf.util.encodeString(data.base64));
+          const model = await mobilenet.load();
+          const prediction = await model.classify(imageTensor);
+          // Detect text
+          const result = await TextRecognition.recognize({
+            uri: data.uri,
+            quality: 0.8,
+            recognitionLanguage: 'en',
+          });
+          setText(result.text);
           setPhoto(data.uri);
+          setPredictions(prediction);
         }
       };
       return (
@@ -37,6 +67,16 @@ const Home = (props) => {
                   setPhoto(null)
                 }}
                 />
+                {text ? (
+                  <Text style={styles.text}>{text}</Text>
+                ) : (
+                  <Text style={styles.message}>No text detected.</Text>
+                )}
+              <View style={styles.predictions}>
+                {predictions.map((prediction) => (
+                  <Text key={prediction.className}>{prediction.className}</Text>
+                ))}
+              </View>
             </View>
             </View>
           ) : (
@@ -131,6 +171,28 @@ const styles = StyleSheet.create({
     photo: {
       width: 300,
       height: 300,
+    },
+    text: {
+      margin: 20,
+      textAlign: 'center',
+      fontSize: 16,
+    },
+    message: {
+      marginTop: 20,
+      marginBottom: 20,
+      textAlign: 'center',
+      fontSize: 16,
+      color: 'gray',
+    },
+    predictions: {
+      position: 'absolute',
+      bottom: 50,
+      left: 0,
+      right: 0,
+      backgroundColor: '#fff',
+      padding: 20,
+      maxHeight: 150,
+      overflow: 'scroll',
     },
     });
         
